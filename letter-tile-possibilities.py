4a2c73a8b703a605c729879920e7fefdb575b57a
PUVIYARASAN-A <puviyarasana.bec23@rathinam.in>
2025-02-17 21:28:36 +0530

    Create letter-tile-possibilities.py

---
+++ letter-tile-possibilities.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        def backtrack(counter):
+            count = 0
+            for char in counter:
+                if counter[char] > 0:
+                    count += 1
+                    counter[char] -= 1
+                    count += backtrack(counter)
+                    counter[char] += 1
+            return count
+
+        return backtrack(Counter(tiles))
